39d9efd42f921959e887c228a95838b9dc63baf3
Tommer <137009790+gwchar2@users.noreply.github.com>
2024-04-02 10:11:32 +0300

    Update error_handler.c

--- lib/error_handler.c
+++ lib/error_handler.c
@@ -4,37 +4,37 @@ void error_manager(ErrorCode errorCode) {
     err_flag++;
     switch (errorCode) {
         case ERR_UNDEFINED_REGISTER:
-            fprintf(stderr, "Error: Undefined register name\n");
+            fprintf(stderr, "Error: Undefined register name in line %d\n", curr_line_number);
             break;
         case ERR_UNDEFINED_COMMAND:
-            fprintf(stderr, "Error: Undefined command name\n");
+            fprintf(stderr, "Error: Undefined command name in line %d\n", curr_line_number);
             break;
         case ERR_EXTRANEOUS_TEXT:
-            fprintf(stderr, "Error: Extraneous text after end of command\n");
+            fprintf(stderr, "Error: Extraneous text after end of command in line %d\n", curr_line_number);
             break;
         case ERR_UNDEFINED_ARGUMENT:
-            fprintf(stderr, "Error: Undefined argument\n");
+            fprintf(stderr, "Error: Undefined argument in line %d\n", curr_line_number);
             break;
         case ERR_MISSING_ARGUMENT:
-            fprintf(stderr, "Error: Missing argument\n");
+            fprintf(stderr, "Error: Missing argument in line %d\n", curr_line_number);
             break;
         case ERR_ILLEGAL_COMMA:
-            fprintf(stderr, "Error: Illegal comma\n");
+            fprintf(stderr, "Error: Illegal comma in line %d\n", curr_line_number);
             break;
         case ERR_MISSING_COMMA:
-            fprintf(stderr, "Error: Missing comma\n");
+            fprintf(stderr, "Error: Missing comma in line %d\n", curr_line_number);
             break;
         case ERR_MULTIPLE_CONSECUTIVE_COMMAS:
-            fprintf(stderr, "Error: Multiple consecutive commas\n");
+            fprintf(stderr, "Error: Multiple consecutive commas in line %d\n", curr_line_number);
             break;
         case ERR_SEGMENTATION_FAULT:
-            fprintf(stderr, "Error: Segmentation fault\n");
+            fprintf(stderr, "Error: Segmentation fault in line %d\n", curr_line_number);
             break;
         case ERR_REDEFINITION_MACRO:
-            fprintf(stderr, "Error: Redefinition of macro name\n");
+            fprintf(stderr, "Error: Redefinition of macro name in line %d\n", curr_line_number);
             break;
         default:
-            fprintf(stderr, "Error: Unknown error\n");
+            fprintf(stderr, "Error: Unknown error in line %d\n", curr_line_number);
             break;
     }
 }
