a7260a629142a40a9284d8117f3dc611361d10d5
MichalRabby <158276482+MichalRabby@users.noreply.github.com>
2024-04-07 14:11:03 +0300

    Update assembler.h

--- include/assembler.h
+++ include/assembler.h
@@ -247,7 +247,7 @@ int check_alpha(char *pointer); /* Checks if a string is all alphabetical letter
 /************************************
 ***** COMMAND PARSER FUNCTIONS ******
 *************************************/
-void check_command() ;
+void check_command(char *input) ;
 int valid_command_name(char *cmd);
 void getNumOfVars();
 int sourceOpCheck(char *token);
