505be5f3cb1a81cf22dc59c654a7639b71982133
Tommer <137009790+gwchar2@users.noreply.github.com>
2024-04-09 10:36:48 +0300

    Update mainAssembler.c

--- src/mainAssembler.c
+++ src/mainAssembler.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include "../include/assembler.h"
 
 
@@ -12,7 +8,7 @@ char *commands[NUM_OF_CMDS] = {
 };    
 
 char *registers[NUM_OF_REGS] = {
-    "r0", "r1","r2","r3","r4","r5",
+    "r1","r2","r3","r4","r5",
     "r6","r7" ,"PSW","PC"
 };
 
@@ -21,64 +17,110 @@ int curr_line_number = 1;
 int IC = 1;
 int DC = 1;
 int err_flag = 0;
-label_node *cmd_label_head = NULL;
+ErrorCode errorCode = 0;                                                            /* Global error variable */
+char *rest_of_line = NULL;                                                          /* this pointer will always pont to the rest of the input line that wans't proccessed yet. */
+char *curr_file;                                                                    /* This pointer will always point to the current file that is open. */
+label_node *cmd_label_head = NULL;                                                  /* A pointer to all labels that represent a command */
 label_node *lbl_head = NULL;                                                        /* Label table head */
-cmd_node *cmd_head = NULL;                                                          /* Instruction segment head */
 label_node *entry_head = NULL;                                                      /* Entry list head */
 label_node *extern_head = NULL;                                                     /* Extern list head */
 label_node *dc_head = NULL;                                                         /* Data segment list head */
-ErrorCode errorCode = 0;                                                            /* Global error variable */
-cmd_node *new_cmd = NULL;
-char *rest_of_line = NULL;                                                          /* this pointer will always pont to the rest of the input line that wans't proccessed yet. */
+cmd_node *cmd_head = NULL;                                                          /* Instruction segment head */
+cmd_node *new_cmd = NULL;                                                           
+macro *head = NULL; 
 
 int main(int argc,char *argv[]) {
     
-    FILE *fp ;
-    char *clean_file_name = (char*)malloc(FILE_NAME_LEN) ;                          /* string to hold the name as recieved in command line. no endings. */
-    int file_count = 0 ;                                                            /* pointer to argv index */
+    FILE *fp;
+    char *clean_file_name = (char*)malloc(FILE_NAME_LEN);                          /* string to hold the name as recieved in command line. no endings. */
+    int file_count = 0;                                                             
     check_allocation(clean_file_name);
     
     
-    /* checking arguments */
-    if (argc < MIN_ARGV){                                                           /* no arguments */ 
-        fprintf(stderr, "error: no arguments.\n");
-        exit(1);
+    /* Checking if we received files to read from! */
+    if (argc < MIN_ARGV){                                                        
+        error(ERR_FILE_ARGS);
+        exit(EXIT_FAILURE);
     }
-    file_count++ ;                                                                  /* argumants found. increase file count */
-    strcpy(clean_file_name, argv[1]); /* getting file name */
+    for (file_count = 1; file_count < argc; file_count++ ) {                                                              
+        strcpy(clean_file_name, argv[file_count]);                                           /* clean_file_name is the name of the file without any ending */
+        
+        /* Start the process by trying to read the .as file */
+        fp = openFile(clean_file_name,0);
+        if (fp == NULL)
+            continue;
+        else{ 
+            /* PreAssembler */
+            preAssembler(fp,clean_file_name) ;
+            fclose(fp);
 
-    
-    /* PreAssembler */
-    fp = openFile(clean_file_name,0);
-    preAssembler(fp,clean_file_name) ;
-    fclose(fp);
+            /* Before we begin, we must note the following:
+            Note lines ( ';' ) and empty lines in the file, will not be counted toward IC, DC or as lines in the file!!! */
 
-    /* First & Second Rotation (FR Flag) */
-    /* Before we begin, we must note the following:
-    Note lines ( ';' ) and empty lines in the file, will not be counted toward IC, DC or as lines in the file!!! */
-    
-    fp = openFile(clean_file_name,1);
-    
-    scan_file(fp);
+            /* Handling and translating the code */
+            scan_file(clean_file_name);
+            fclose(fp);
 
+            /* Fix the entry labels to hold the correct addresses */
+            fixEntrys();
 
+             /* Fix the addresses in the Data segment & Labels used in Instruction segment, and lastly, make the files. */
+            mergeSegments();
+
+                /*if (!fixCMDs()){ 
+                    continue;
+                    If there is a label that is used, but it has no label node --> error(ERR_LBL_USE)!!!;
+                 }; 
+                 In this stage: we do the following:
+                 if label used is external -> we add a row to the specific external in extern_list
+                 switch label values with updated values from DC + label table. (i think only data segment and command labels matter?)
+                 */
+/* משימות להמשך 
+לעדכן הוספת שורה ללייבלים של אקסטרנל
+לעדכן סימני שאלה ברשימת פקודות
+לעדכן הדפסת פקודות לקובץ אובגקט שורות 138-165 ב
+file_handler
+freeLists() */
+            /* If we have errors, dont create files, continue to translate next file given by user! */
+            if (err_flag != 0){
+                error(ERR_ERR_FLAG);
+                continue;
+            }
+            else {
+                makefiles(clean_file_name);
+            }
+        }
+    }
+        /* The following are just semi-lists (pointers to lists in lists) */
+        /*cmd_label_head = NULL;                                           
+        entry_head = NULL;                                                     
+        extern_head = NULL;                                                    
+        dc_head = NULL; 
+        curr_line_number = 1;
+        IC = 1;
+        DC = 1;*
+        err_flag = 0;
+        /* We will free everything here using the official heads of lists    
+        free_lists();   */
+
+/***********************************************************/
+/* DELETE THIS SECTION !!!! */
+/**********************************************************/
+    printf("IC: %d\t DC: %d\n",IC,DC);
     if (lbl_head != NULL)
         printList(1);
+    printf("********************************************\n");
     if (extern_head != NULL)
         printf("\n\nExtern head is: [%p] [%s]\n",(void*)extern_head,extern_head->label_name);
-    printf("Next DC is: %d\n\n",DC);
     if (dc_head != NULL){
         printf("DC head is: [%p] [%s] printing list: \n",(void*)dc_head,dc_head->label_name);
         printList(2);
     }
-    /*if (cmd_head != NULL){
+    if (cmd_head != NULL){
         printf("CMD head is: [%p] printing list: \n",(void*)cmd_head);
         printList(3);
-    }*/
+    }
     
-    /*printf("\nENTRY HEAD NAME: %s\n",entry_head -> label_name);*/
-    /*printf("EXTERN HEAD NAME: %d\n",extern_head -> label_name);*/
-    /*printf("\CMD HEAD NAME: %s\n",cmd_label_head -> label_name);*/
     return 0;
 }
 
