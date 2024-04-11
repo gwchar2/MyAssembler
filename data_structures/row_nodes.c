726200459860f1f6c94411e32e56cd77284be616
Tommer <137009790+gwchar2@users.noreply.github.com>
2024-04-12 00:21:58 +0300

    Update row_nodes.c

--- data_structures/row_nodes.c
+++ data_structures/row_nodes.c
@@ -4,7 +4,7 @@
 /*
 *   This function creates a row_node
 */
-row_node *create_row(int address){
+row_node *create_row(int address,label_node *label_node){
     row_node *new_row = malloc(sizeof(row_node));                           /* Allocates memory for the new node */
     check_allocation(new_row);                                              /* Checks to see if the memory was allocated correctly */
     new_row -> address = address; 
@@ -15,19 +15,22 @@ row_node *create_row(int address){
 /*
 *   This function adds a row to the specific label node 
 */
-void *add_row(label_node *cur_label, int address) {
-    row_node *new_row = create_row(address); 
-    row_node *cur_row = cur_label -> row_list;
-
-    if (cur_row -> address == address ) 
-            return (void*)new_row;
-    while (cur_row -> next_row != NULL ){
-        if (cur_row -> next_row -> address == address ) 
-            return (void*)new_row;
-        else
-            cur_row = cur_row -> next_row;
+void *add_row(label_node *label_node, int address) {
+    row_node *new_row = create_row(address,label_node); 
+    row_node *current = cur_label -> row_list;
+    
+    if (label_node -> row_list == NULL)
+        label_node -> row_list = new_row;
+    else if (current -> address == address)
+        return (void*)current;
+    else {
+        while (current -> next_row != NULL ){
+            if (current -> next_row -> address == address ) 
+                return (void*)current->next_row;
+            else
+                cur_row = cur_row -> next_row;
+        }
+        cur_row -> next_row = new_row;
     }
-    cur_row -> next_row = new_row;
-
     return (void*)new_row;
 }
